463d46e47fb27747dc4e6b45d7e6ef786ce1063c
Mell Basham <mellbasham@gmail.com>
2020-11-26 02:36:09 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,7 +2,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
    
-   array.count{ |strings| strings == "" }
+   array.count{ |strings| strings  }
  end
 
 def count_empty_strings(array)
